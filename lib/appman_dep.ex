c0cd89bb405f48ff7277067af50c33c033400309
Anton Chernev <ss.a.chernev@gmail.com>
2019-07-02 23:06:40 +0300

    Add main connector communication

--- lib/appman_dep.ex
+++ lib/appman_dep.ex
@@ -1,18 +1,2 @@
 defmodule AppmanDep do
-  @moduledoc """
-  Documentation for AppmanDep.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> AppmanDep.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/appman_dep/connector.ex
+++ lib/appman_dep/connector.ex
@@ -2,11 +2,19 @@ defmodule AppmanDep.Connector do
   use GenServer
 
   def start_link(_args) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, [])
   end
 
   def init(_args) do
-    IO.puts("Hello")
+    manager_name = Node.self() |> to_string() |> String.split("-") |> List.first()
+    manager_node = String.to_atom(manager_name <> "@localhost")
+    Node.connect(manager_node)
+    GenServer.cast({:global, manager_name}, {:node_running, Node.self(), self()})
     {:ok, nil}
+  end
+
+  def handle_cast(:stop, state) do
+    System.stop()
+    {:noreply, state}
   end
 end
