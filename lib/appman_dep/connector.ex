5014cee35c722ede702feb3bd4b4f62033cab0cb
Anton Chernev <ss.a.chernev@gmail.com>
2019-07-03 18:44:59 +0300

    Fixes to connector

--- lib/appman_dep/connector.ex
+++ lib/appman_dep/connector.ex
@@ -6,15 +6,34 @@ defmodule AppmanDep.Connector do
   end
 
   def init(_args) do
-    manager_name = Node.self() |> to_string() |> String.split("-") |> List.first()
-    manager_node = String.to_atom(manager_name <> "@localhost")
+    manager_name = Node.self() |> to_string() |> String.split("_") |> List.first()
+    manager_node = String.to_atom(manager_name <> "@127.0.0.1")
     Node.connect(manager_node)
-    GenServer.cast({:global, manager_name}, {:node_running, Node.self(), self()})
-    {:ok, nil}
+    Process.sleep(1000)
+    GenServer.cast({:global, Appman.Manager}, {:node_running, Node.self(), self()})
+    Process.send_after(self(), :server_timeout, 3000)
+    {:ok, false}
   end
 
   def handle_cast(:stop, state) do
     System.stop()
     {:noreply, state}
   end
+
+  def handle_cast({:server_pid, pid}, _state) do
+    Process.monitor(pid)
+    {:noreply, true}
+  end
+
+  def handle_info({:DOWN, ref, :process, _, _}, state) when Kernel.is_reference(ref) do
+    System.stop()
+    {:noreply, state}
+  end
+
+  def handle_info(:server_timeout, false) do
+    System.stop()
+    {:noreply, false}
+  end
+
+  def handle_info(:server_timeot, true), do: {:noreply, true}
 end
