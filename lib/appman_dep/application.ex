b2cbd6e6e8d326f5f43e807f30ba738ab844b70e
Anton Chernev <ss.a.chernev@gmail.com>
2019-07-04 16:55:46 +0300

    Logger config

--- lib/appman_dep/application.ex
+++ lib/appman_dep/application.ex
@@ -4,6 +4,16 @@ defmodule AppmanDep.Application do
   use Application
 
   def start(_type, _args) do
+    log_path =
+      "/var/log/appman/" <>
+        (Node.self() |> to_string() |> String.split("@") |> List.first()) <>
+        ".log"
+
+    Logger.configure_backend(
+      {LoggerFileBackend, :default_log},
+      path: log_path
+    )
+
     children = [
       {AppmanDep.Connector, []}
     ]
--- lib/appman_dep/connector.ex
+++ lib/appman_dep/connector.ex
@@ -2,7 +2,7 @@ defmodule AppmanDep.Connector do
   use GenServer
 
   def start_link(_args) do
-    GenServer.start_link(__MODULE__, [])
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_args) do
